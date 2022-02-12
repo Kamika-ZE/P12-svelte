bdee8043e259efc4d560781b6f4ad1efed31856c
Gino Chasles <gchasles@mgstream.com>
2022-02-12 13:43:38 +0100

    modif gino

--- src/routes/Index.svelte
+++ src/routes/Index.svelte
@@ -1,14 +1,10 @@
 <script>
-	import Header from '../components/Header.svelte';
 	import Main from '../components/Main.svelte';
-	import Footer from '../components/Footer.svelte';
 </script>
 
-<Header/>
 
 <Main/>
 
-<Footer/>
 
 <style>
 
